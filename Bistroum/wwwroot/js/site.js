2d10afbc53ce9cb01e8e95cd287e5d9e3f9e0366
Aleksander Krücken <nabiralnik7@yahoo.com>
2018-06-06 21:59:52 +0200

    Dodana funkcija za reset abakusa

--- Bistroum/wwwroot/js/site.js
+++ Bistroum/wwwroot/js/site.js
@@ -21,19 +21,45 @@
 */
 }
 
+// Ponastavi kroglice na zacetno pozicijo
+function reset() {
+    for (var i = 1; i <= n; i++) {
+        document.getElementById(10 * i + 5).setAttribute('cy', yUp);
+        for (var j = 1; j <= 4; j++) {
+            document.getElementById(10 * i + j).setAttribute('cy', yStart + (j - 1) * yDiff);
+        }
+    }
+}
 
 function generiranjeSorobana() {
     var vrstice = '';
+
+    vrstice = vrstice + "<svg id='sorobanSVG' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 800 400' >";
+
+    // Izris okvirja
+    vrstice = vrstice + "<rect id='okvir' x='0' y='0' width='100%' height='100%' />";
+    vrstice = vrstice + "<rect id='resetGumb' x='715' y='2' rx='5' ry='5' width='70' height='13' />";
+    vrstice = vrstice + "<text id='resetTekst' x='720' y='13' fill='red'>Ponastavi</text>";
+    vrstice = vrstice + "<line x1='0' y1='123' x2='800' y2='123' />";
+    vrstice = vrstice + "<circle cx='200' cy='123' r='2' />";
+    vrstice = vrstice + "<circle cx='500' cy='123' r='2' />";
+
+
+    // Looping za kroglice petice (narise vse krogce v zgornji vrstici)
     for (var i = 1; i <= n; i++) {
         vrstice = vrstice + "<ellipse id='" + (i * 10 + 5) + "' cx='" + (xStart + (n - i) * xDiff) + "' cy ='" + yUp + "' rx='" + aElipse + "' ry ='" + bElipse + "' />";
-//        <ellipse id=i* 10 + 5 cx = xStart + (n - i) * xDiff cy = yUp rx = aElipse ry = bElipse />
-        for (var j = 1; i <= 4; i++) {
-//            <ellipse id=i* 10 + j cx = xStart + (n - i) * xDiff cy = yStart + (j - 1) * yDiff rx = aElipse ry = bElipse />
+        // Looping za kroglice enice (narise vse kroglice v spodnjih 4-ih vrsticah
+        for (var j = 1; j <= 4; j++) {
+            vrstice = vrstice + "<ellipse id='" + (i * 10 + j) + "' cx='" + (xStart + (n - i) * xDiff) + "' cy ='" + (yStart + (j - 1) * yDiff) + "' rx='" + aElipse + "' ry ='" + bElipse + "' />";
         }
     }
 
+    vrstice = vrstice + "</svg>";
     // Napolnitev SVG elementa
-    document.getElementById("sorobanSVG").innerHTML = vrstice;
+    //document.getElementById("sorobanSVG").innerHTML = vrstice; // Ne dela v IE
+    //$('#sorobanSVG').html(vrstice);  
+    $('#DivSvg').html(vrstice); 
+
 }
 
 
@@ -45,6 +71,9 @@ function poslusajDogodke() {
         celice[i].addEventListener("mousedown", pritisk);
     };
 
+    // Za reset gumb
+    document.getElementById("resetGumb").addEventListener("mousedown", reset);
+    document.getElementById("resetTekst").addEventListener("mousedown", reset);
 }
 
 
